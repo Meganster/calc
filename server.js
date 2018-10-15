d1071b541edac3e3609b2b181c714d7f1a8aefcc
Meganster <ppro100takk@mail.ru>
2018-10-15 18:34:54 +0300

    Add functional & cosmetic

--- server.js
+++ server.js
@@ -23,92 +23,21 @@ module.exports = http.createServer((req, res) => {
         if (pathname === '/send') {
             let dataFromClient = '';
 
-              // A chunk of data has been recieved.
-              req.on('data', (chunk) => {
-                  dataFromClient += chunk;
-              });
+            // A chunk of data has been recieved.
+            req.on('data', (chunk) => {
+                dataFromClient += chunk;
+            });
 
-              // The whole response has been received.
-              req.on('end', () => {
-                  console.log(dataFromClient);
-              });
+            // The whole response has been received.
+            req.on('end', () => {
+                dataFromClient = JSON.parse(dataFromClient);
+                console.log(dataFromClient);
+            });
 
-              res.statusCode = 200;
+            res.statusCode = 200;
         }
     }
 });
-
-
-function receiveFile(filepath, req, res) {
-
-    // non-streaming client sends ctx
-    if (req.headers['content-length'] > config.limitFileSize) {
-        res.statusCode = 413;
-        res.end('File is too big!');
-        return;
-    }
-
-    let size = 0;
-
-    let writeStream = new fs.WriteStream(filepath, {flags: 'wx'});
-
-    req
-        .on('data', chunk => {
-            size += chunk.length;
-
-            if (size > config.limitFileSize) {
-                // early connection close before recieving the full request
-
-                res.statusCode = 413;
-
-                // if we just res.end w/o connection close, browser may keep on sending the file
-                // the connection will be kept alive, and the browser will hang (trying to send more data)
-                // ctx header tells node to close the connection
-                // also see http://stackoverflow.com/questions/18367824/how-to-cancel-http-upload-from-data-events/18370751#18370751
-                res.setHeader('Connection', 'close');
-
-                // Some browsers will handle this as 'CONNECTION RESET' error
-                res.end('File is too big!');
-
-                writeStream.destroy();
-                fs.unlink(filepath, err => { // eslint-disable-line
-                    /* ignore error */
-                });
-
-            }
-        })
-        .on('close', () => {
-            writeStream.destroy();
-            fs.unlink(filepath, err => { // eslint-disable-line
-                /* ignore error */
-            });
-        })
-        .pipe(writeStream);
-
-    writeStream
-        .on('error', err => {
-            if (err.code === 'EEXIST') {
-                res.statusCode = 409;
-                res.end('File exists');
-            } else {
-                console.error(err);
-                if (!res.headersSent) {
-                    res.writeHead(500, {'Connection': 'close'});
-                    res.end('Internal error');
-                } else {
-                    res.end();
-                }
-                fs.unlink(filepath, err => { // eslint-disable-line
-
-                });
-            }
-
-        })
-        .on('close', () => {
-            res.end('OK');
-
-        });
-}
 
 function sendFile(filepath, res) {
     let fileStream = fs.createReadStream(filepath);
