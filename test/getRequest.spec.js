4c708b0bc68b80f8c2c23838af23fc80de3e546d
Meganster <ppro100takk@mail.ru>
2018-10-18 18:56:50 +0300

    Add coverage

--- test/getRequest.spec.js
+++ test/getRequest.spec.js
@@ -183,6 +183,36 @@ describe('Calculator testing', () => {
         );
     });
 
+    it('Should base index.html', (done) => {
+        return http.get('http://localhost:3333', (resp) => {
+            let dataFromServer = '';
+
+            // A chunk of data has been recieved.
+            resp.on('data', (chunk) => {
+                dataFromServer += chunk;
+            });
+
+            // The whole response has been received.
+            resp.on('end', () => {
+                assert(dataFromServer.length != 0);
+
+                fs.readFile("./public/index.html", 'utf-8', function (err, indexFile) {
+                    if (err) {
+                        console.log("Error read file ./public/index.html!");
+                        assert(false);
+                        done();
+                    }
+
+                    assert(dataFromServer.localeCompare(indexFile) === 0);
+                    done();
+                });
+            });
+        }).on("error", (err) => {
+            console.log("Error in GET request http://localhost:3333!");
+            assert(false);
+            done();
+        });
+    });
 
 
     after((done) => {
