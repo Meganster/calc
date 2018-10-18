bf4dbf8841611e29a18e27e08c09dfe622bae709
Meganster <ppro100takk@mail.ru>
2018-10-18 18:32:57 +0300

    Uncomment tests

--- test/getRequest.spec.js
+++ test/getRequest.spec.js
@@ -15,103 +15,19 @@ describe('Calculator testing', () => {
         });
     });
 
-    // it('0 + 0 = 0', (done) => {
-    //     fetch(`http://localhost:3333/send`, {
-    //         method: "POST",
-    //         body: JSON.stringify({
-    //             x: 0,
-    //             y: 0,
-    //             operation: '+'
-    //         })
-    //     }).then(
-    //         (res) => {
-    //             res.json().then(
-    //                 (body) => {
-    //                     assert(body.result === 0);
-    //                     done();
-    //                 },
-    //                 () => {
-    //                     assert(false);
-    //                     done();
-    //                 }
-    //             );
-    //         },
-    //         (err) => {
-    //             assert(false);
-    //             done();
-    //         }
-    //     );
-    // });
-    //
-    // it('0 - 0 = 0', (done) => {
-    //     fetch(`http://localhost:3333/send`, {
-    //         method: "POST",
-    //         body: JSON.stringify({
-    //             x: 0,
-    //             y: 0,
-    //             operation: '-'
-    //         })
-    //     }).then(
-    //         (res) => {
-    //             res.json().then(
-    //                 (body) => {
-    //                     assert(body.result === 0);
-    //                     done();
-    //                 },
-    //                 () => {
-    //                     assert(false);
-    //                     done();
-    //                 }
-    //             );
-    //         },
-    //         (err) => {
-    //             assert(false);
-    //             done();
-    //         }
-    //     );
-    // });
-    //
-    // it('0 * 0 = 0', (done) => {
-    //     fetch(`http://localhost:3333/send`, {
-    //         method: "POST",
-    //         body: JSON.stringify({
-    //             x: 0,
-    //             y: 0,
-    //             operation: '*'
-    //         })
-    //     }).then(
-    //         (res) => {
-    //             res.json().then(
-    //                 (body) => {
-    //                     assert(body.result === 0);
-    //                     done();
-    //                 },
-    //                 () => {
-    //                     assert(false);
-    //                     done();
-    //                 }
-    //             );
-    //         },
-    //         (err) => {
-    //             assert(false);
-    //             done();
-    //         }
-    //     );
-    // });
-
-    it('0 / 0 = error', (done) => {
+    it('0 + 0 = 0', (done) => {
         fetch(`http://localhost:3333/send`, {
             method: "POST",
             body: JSON.stringify({
                 x: 0,
                 y: 0,
-                operation: '/'
+                operation: '+'
             })
         }).then(
             (res) => {
                 res.json().then(
                     (body) => {
-                        //assert(body.result === null);
+                        assert(body.result === 0);
                         done();
                     },
                     () => {
@@ -127,61 +43,145 @@ describe('Calculator testing', () => {
         );
     });
 
-    // it('0 / 1 = 0', (done) => {
-    //     fetch(`http://localhost:3333/send`, {
-    //         method: "POST",
-    //         body: JSON.stringify({
-    //             x: 0,
-    //             y: 1,
-    //             operation: '/'
-    //         })
-    //     }).then(
-    //         (res) => {
-    //             res.json().then(
-    //                 (body) => {
-    //                     assert(body.result === 0);
-    //                     done();
-    //                 },
-    //                 () => {
-    //                     assert(false);
-    //                     done();
-    //                 }
-    //             );
-    //         },
-    //         (err) => {
-    //             assert(false);
-    //             done();
-    //         }
-    //     );
-    // });
-    //
-    // it('10 / 1 = 10', (done) => {
-    //     fetch(`http://localhost:3333/send`, {
-    //         method: "POST",
-    //         body: JSON.stringify({
-    //             x: 10,
-    //             y: 1,
-    //             operation: '/'
-    //         })
-    //     }).then(
-    //         (res) => {
-    //             res.json().then(
-    //                 (body) => {
-    //                     assert(body.result === 10);
-    //                     done();
-    //                 },
-    //                 () => {
-    //                     assert(false);
-    //                     done();
-    //                 }
-    //             );
-    //         },
-    //         (err) => {
-    //             assert(false);
-    //             done();
-    //         }
-    //     );
-    // });
+    it('0 - 0 = 0', (done) => {
+        fetch(`http://localhost:3333/send`, {
+            method: "POST",
+            body: JSON.stringify({
+                x: 0,
+                y: 0,
+                operation: '-'
+            })
+        }).then(
+            (res) => {
+                res.json().then(
+                    (body) => {
+                        assert(body.result === 0);
+                        done();
+                    },
+                    () => {
+                        assert(false);
+                        done();
+                    }
+                );
+            },
+            (err) => {
+                assert(false);
+                done();
+            }
+        );
+    });
+
+    it('0 * 0 = 0', (done) => {
+        fetch(`http://localhost:3333/send`, {
+            method: "POST",
+            body: JSON.stringify({
+                x: 0,
+                y: 0,
+                operation: '*'
+            })
+        }).then(
+            (res) => {
+                res.json().then(
+                    (body) => {
+                        assert(body.result === 0);
+                        done();
+                    },
+                    () => {
+                        assert(false);
+                        done();
+                    }
+                );
+            },
+            (err) => {
+                assert(false);
+                done();
+            }
+        );
+    });
+
+    it('0 / 0 = error', (done) => {
+        fetch(`http://localhost:3333/send`, {
+            method: "POST",
+            body: JSON.stringify({
+                x: 0,
+                y: 0,
+                operation: '/'
+            })
+        }).then(
+            (res) => {
+                res.json().then(
+                    (body) => {
+                        assert(body.result === "error");
+                        done();
+                    },
+                    () => {
+                        assert(false);
+                        done();
+                    }
+                );
+            },
+            (err) => {
+                assert(false);
+                done();
+            }
+        );
+    });
+
+    it('0 / 1 = 0', (done) => {
+        fetch(`http://localhost:3333/send`, {
+            method: "POST",
+            body: JSON.stringify({
+                x: 0,
+                y: 1,
+                operation: '/'
+            })
+        }).then(
+            (res) => {
+                res.json().then(
+                    (body) => {
+                        assert(body.result === 0);
+                        done();
+                    },
+                    () => {
+                        assert(false);
+                        done();
+                    }
+                );
+            },
+            (err) => {
+                assert(false);
+                done();
+            }
+        );
+    });
+
+    it('10 / 1 = 10', (done) => {
+        fetch(`http://localhost:3333/send`, {
+            method: "POST",
+            body: JSON.stringify({
+                x: 10,
+                y: 1,
+                operation: '/'
+            })
+        }).then(
+            (res) => {
+                res.json().then(
+                    (body) => {
+                        assert(body.result === 10);
+                        done();
+                    },
+                    () => {
+                        assert(false);
+                        done();
+                    }
+                );
+            },
+            (err) => {
+                assert(false);
+                done();
+            }
+        );
+    });
 
     after((done) => {
         app.close(() => {
